969c89f8057e1981c4bccc2aa8f17bedb250dab1
Frank <frankivil89@gmail.com>
2024-02-17 20:59:01 -0500

    Correciones a la pagina de producto

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import '../css/Productos.css';
+
+//productos para mockup
 import Perro from '../img/perro1.png';
+import Alimento1 from '../img/alimento1.jpeg';
+import Alimento2 from '../img/alimento2.jpeg';
+import Alimento3 from '../img/alimento3.jpeg';
+import Dentalife from '../img/dentalife.jpeg';
+import DentaStix from '../img/dentastix.jpeg';
+import Locion from '../img/locion.jpeg';
+import Nextgard from '../img/nextgard.jpeg';
 
 const Productos = () => {
 	return (
@@ -16,52 +25,52 @@ const Productos = () => {
 			<div id='page'>
 				<div id='producto-lista'>
 					<div class='producto'>
-						<img src={Perro}></img>
+						<img src={Perro} alt='panholetaPerro'></img>
 						<p id='categ-producto'>Juguetes y pañoletas</p>
 						<p id='nombre-producto'>Pañoleta superheroe</p>
 						<p id='precio-producto'>COP$ 16.900</p>
 					</div>
 					<div class='producto'>
-						<img src={Perro}></img>
-						<p id='categ-producto'>Juguetes y pañoletas</p>
-						<p id='nombre-producto'>Pañoleta superheroe</p>
-						<p id='precio-producto'>COP$ 16.900</p>
+						<img src={Alimento1} alt='alimento1'></img>
+						<p id='categ-producto'>Alimentos</p>
+						<p id='nombre-producto'>DogChow Alimento</p>
+						<p id='precio-producto'>COP$ 25.900</p>
 					</div>
 					<div class='producto'>
-						<img src={Perro}></img>
-						<p id='categ-producto'>Juguetes y pañoletas</p>
-						<p id='nombre-producto'>Pañoleta superheroe</p>
-						<p id='precio-producto'>COP$ 16.900</p>
+						<img src={Alimento2} alt='alimento2'></img>
+						<p id='categ-producto'>Alimentos</p>
+						<p id='nombre-producto'>Doncan Alimento</p>
+						<p id='precio-producto'>COP$ 29.900</p>
 					</div>
 					<div class='producto'>
-						<img src={Perro}></img>
+						<img src={Alimento3} alt='alimento3'></img>
 						<p id='categ-producto'>Juguetes y pañoletas</p>
 						<p id='nombre-producto'>Pañoleta superheroe</p>
-						<p id='precio-producto'>COP$ 16.900</p>
+						<p id='precio-producto'>COP$ 31.900</p>
 					</div>
 					<div class='producto'>
-						<img src={Perro}></img>
-						<p id='categ-producto'>Juguetes y pañoletas</p>
-						<p id='nombre-producto'>Pañoleta superheroe</p>
-						<p id='precio-producto'>COP$ 16.900</p>
+						<img src={Dentalife} alt='dentalife'></img>
+						<p id='categ-producto'>Medicamentos</p>
+						<p id='nombre-producto'>Dentalife</p>
+						<p id='precio-producto'>COP$ 82.900</p>
 					</div>
 					<div class='producto'>
-						<img src={Perro}></img>
-						<p id='categ-producto'>Juguetes y pañoletas</p>
-						<p id='nombre-producto'>Pañoleta superheroe</p>
-						<p id='precio-producto'>COP$ 16.900</p>
+						<img src={DentaStix} alt='dentastix'></img>
+						<p id='categ-producto'>Medicamentos</p>
+						<p id='nombre-producto'>DentaStix</p>
+						<p id='precio-producto'>COP$ 74.900</p>
 					</div>
 					<div class='producto'>
-						<img src={Perro}></img>
-						<p id='categ-producto'>Juguetes y pañoletas</p>
-						<p id='nombre-producto'>Pañoleta superheroe</p>
-						<p id='precio-producto'>COP$ 16.900</p>
+						<img src={Locion} alt='locion'></img>
+						<p id='categ-producto'>Estética</p>
+						<p id='nombre-producto'>Loción para mascotas</p>
+						<p id='precio-producto'>COP$ 33.900</p>
 					</div>
 					<div class='producto'>
-						<img src={Perro}></img>
-						<p id='categ-producto'>Juguetes y pañoletas</p>
-						<p id='nombre-producto'>Pañoleta superheroe</p>
-						<p id='precio-producto'>COP$ 16.900</p>
+						<img src={Nextgard} alt='nextgard'></img>
+						<p id='categ-producto'>Medicamentos</p>
+						<p id='nombre-producto'>NextGuard</p>
+						<p id='precio-producto'>COP$ 101.700</p>
 					</div>
 				</div>
 			</div>
